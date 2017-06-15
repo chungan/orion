d739aeb25e0b53a3c7a91fd2c705d2b755c77b40
jlin703 <jonathan.lin.703@gmail.com>
2017-06-14 21:34:25 -0700

    Implemented basic linear regression

---
+++ linear_regression.py
@@ -0,0 +1,27 @@
+from training_data import ratings_for_movie, specific_user_rating, all_ratings
+
+def predict(userID, movieID, all_ratings):
+    '''
+    Predicts a user's rating for a movie.
+    @param userID: the Netflix-assigned ID for the user
+    @type userID: int (between 1 to 2649429, inclusive)
+    @param movieID: the Netflix-assigned ID for the relevant movie
+    @type movieID: int (between 1 and 17770, inclusive)
+    @param all_ratings: the ndarray returned by calling all_ratings
+    @type all_ratings: numpy.ndarray
+
+    @return: the user's predicted rating for the movie
+    @type: float (between 1.0 and 5.0, inclusive)
+    '''
+    main_user_ratings = all_ratings[all_ratings[:, 1] == userID]
+    true_ratings = ratings_for_user[:, 2] #the b-vector
+
+    main_movie_ratings = ratings_for_movie(movieID)
+    all_relevant_ratings = []
+    for (movie, user, rating, date) in main_movie_ratings:
+        ratings = [specific_user_rating(user, m) for m in main_user_ratings[:, 0]]
+        all_relevant_ratings.append(ratings)
+    all_relevant_ratings = np.array(all_relevant_ratings).T #the A-matrix
+
+    weights = np.linalg.lstsq(all_relevant_ratings, true_ratings)[0]
+    return sum(weights * main_movie_ratings[:, 2])
--- training_data.py
+++ training_data.py
@@ -13,9 +13,25 @@ def ratings_for_movie(movieID):
     movie_file = 'mv_%07d.txt'%movieID
     ratings = np.loadtxt(movie_file, delimiter=',', skiprows=1,
                       converters={2: lambda s: datestr2num(s.decode("utf-8"))})
-    #movie_column = np.zeros((ratings.shape[0], 1)) + movieID
     movie_column = np.full((ratings.shape[0], 1), movieID)
     return np.concatenate((movie_column, ratings), axis=1)
+
+def specific_user_rating(userID, movieID):
+    '''
+    Queues rating for specific user for given movie.
+    If doesn't exist, returns average rating for movie.
+    @param userID: the Netflix-assigned ID for the user
+    @type userID: int (between 1 to 2649429, inclusive)
+    @param movieID: the Netflix-assigned ID for the relevant movie
+    @type movieID: int (between 1 and 17770, inclusive)
+
+    @return: a user's rating for the movie, or the average rating
+    @type: float
+    '''
+    movie_ratings = ratings_for_movie(movieID)
+    if userID in movie_ratings[:, 1]:
+        return movie_ratings[movie_ratings[:, 1] == userID][0][2]
+    return np.mean(movie_ratings[:, 2])
 
 def all_ratings():
     '''
@@ -24,4 +40,4 @@ def all_ratings():
     @return: an array containing rating data for all 17770 movies
     @type: numpy.ndarray
     '''
-    return np.concatenate([ratings_for_movie(i) for i in range(1, 17770)])
+    return np.concatenate([ratings_for_movie(i) for i in range(1, 17771)])
